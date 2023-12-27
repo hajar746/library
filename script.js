63efda768b464ae3e3a52aadf5be1bedf0af8bfe
hajar746 <132266440+hajar746@users.noreply.github.com>
2023-12-27 17:12:43 +0300

    basic layout, basic functions

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const myLibrary = [
+  { title: "The Book Theif", author: "idontknow", pages: 1500, read: true },
+  { title: "Atomic Habits", author: "jamessomthing", pages: 500, read: false },
+];
+
+// NEW BOOK FUNCTIONS
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = Boolean(read);
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+const book1 = new Book("The Alchemist", "idontknow", 345, true);
+addBookToLibrary(book1);
+
+console.log(myLibrary);
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    console.log(book);
+  });
+}
