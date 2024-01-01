fd30e72cd42609697f30f48e7c6fd3584fd0a6fb
hajar746 <132266440+hajar746@users.noreply.github.com>
2024-01-01 16:50:22 +0300

    delete and change read status of form(only on card)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const myLibrary = [];
 
-// NEW BOOK CONSTRUCTOR
+// NEW BOOK CONSTRUCTOR ////////////////
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -10,24 +10,34 @@ function Book(title, author, pages, read) {
   this.read = Boolean(read);
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+Book.prototype.addBook = function () {
+  myLibrary.push(this);
   const allBooks = document.querySelector(".books");
   allBooks.insertAdjacentHTML(
     "beforeend",
     `<div class="card">
-        <h2>${book.title}</h2>
-        <p class='author'>by <span>${book.author}</span></p>
-        <p class='pages'>${book.pages} pages</p>
-        <button class='read'>${
-          book.read === true ? "Read" : "Not read"
-        }</button>
-        <button class='edit'>Edit</button>
-      </div>`
+          <img src="imgs/close.png" alt="close-form" class="btn-delete"/>
+          <h2>${this.title}</h2>
+          <p class='author'>by <span>${this.author}</span></p>
+          <p class='pages'>${this.pages} pages</p>
+          <button class='read'>${
+            this.read === true ? "Read" : "Not read"
+          }</button>
+          <button class='edit'>Edit</button>
+        </div>`
   );
-}
+};
 
-// SHOW NEW BOOK FORM
+Book.prototype.readStatus = function () {
+  if (this.read === true) {
+    this.read = false;
+  }
+  if (this.read === false) {
+    this.read = true;
+  }
+};
+
+// SHOW NEW BOOK FORM ////////////
 const btnOpenModal = document.querySelector(".btn-add");
 const modal = document.querySelector(".popup");
 const btnCloseModal = document.querySelector(".btn-close");
@@ -44,9 +54,11 @@ btnCloseModal.addEventListener("click", function () {
   modal.classList.remove("open-popup");
 });
 
-// ADDING NEW BOOK TO LIBRARY
+// ADDING NEW BOOK TO LIBRARY USING FORM INFO ///////////
 const btnAddBook = document.querySelector(".add-book");
 const allInputs = document.querySelectorAll("input");
+const errorMesg = document.querySelector(".error");
+const form = document.querySelector("form");
 
 btnAddBook.addEventListener("click", function (e) {
   let valid = 0;
@@ -61,12 +73,47 @@ btnAddBook.addEventListener("click", function (e) {
     if (input.value !== "" || input.value === true || input.value === false)
       valid++;
   });
-  if (valid < 3) {
-    e.preventDefault();
-  } else {
-    addBookToLibrary(newBook);
+  if (valid > 3) {
+    newBook.addBook();
     appWelcome.style.display = "none";
-    document.querySelector("form").reset();
+    form.reset();
     modal.close();
+    errorMesg.style.display = "none";
+  } else {
+    e.preventDefault();
+    errorMesg.style.display = "block";
+  }
+  console.log(myLibrary);
+});
+
+// MARKING BOOK AS READ/NOT READ /////////
+document.addEventListener("click", function (e) {
+  const target = e.target.closest(".read");
+  if (!target) return;
+
+  if (target && target.textContent === "Read") {
+    target.textContent = "Not read";
+    target.classList.add("not-read");
+    target.style.display = "block";
+  } else {
+    target.textContent = "Read";
+    target.classList.add("already-read");
+    target.classList.remove("not-read");
+    target.style.display = "block";
+  }
+
+  console.log(myLibrary);
+});
+
+// DELETING A BOOK /////////
+document.addEventListener("click", function (e) {
+  const target = e.target.closest(".btn-delete");
+  const book = e.target.closest(".card");
+  const bookTitle = e.target.closest(".title");
+
+  if (target) {
+    const index = myLibrary.findIndex((book) => book.title === bookTitle);
+    myLibrary.splice(index, 1);
+    book.remove();
   }
 });
